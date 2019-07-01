4cf7e2706bc9f274e0064565424fb0a133e308dc
github username <github email address>
2019-06-30 20:38:42 -0700

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+
   def new
     @article = Article.new
   end
@@ -16,6 +17,20 @@ class ArticlesController < ApplicationController
     @article.title = params[:title]
     @article.description = params[:description]
     @article.save
+    redirect_to article_path(@article)
+  end
+
+
+
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    Article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
   end
 
